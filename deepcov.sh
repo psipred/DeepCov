8e88573e686623eeb3d01b5a31afff8301e3cf41
Shaun Kandathil <shaun.mk@gmail.com>
2017-10-23 11:43:48 +0100

    Updated deepcov.sh
    Fixed some paths which were incorrectly set when calling deepcov.sh from another directory

--- deepcov.sh
+++ deepcov.sh
@@ -44,10 +44,9 @@ deepcov_root=$(dirname $0)
 bin=${deepcov_root}/bin
 cov21stats=${bin}/cov21stats
 
-# check that the executable exists.
-# TODO: will this check fail if someone symlinks to this script and invokes the link?
+# check that the cov21stats executable exists.
 if [ ! -s $cov21stats ] || [ ! -x $cov21stats ]; then
-    echo "Unable to find cov21stats executable in ${bin}. Did you run setup.sh?" >&2
+    echo "$0 : Unable to find cov21stats executable in ${bin}. Did you run setup.sh?" >&2
     exit 6
 fi
 
@@ -58,11 +57,16 @@ input_file=''
 output_file=''
 
 ####### Parse commandline options
+# TODO: add an option to provide a list of input filenames, and iterate over them with a given model
 while [[ $# -ge 1 ]]
 do
     key="$1"
     
     case $key in
+	-h|--help)
+	    echo "$help_text"
+	    exit 0
+	    ;;
 	-r|--receptive-field)
 	    rf="$2"
 	    shift # extra shift since this arg has a value
@@ -79,18 +83,13 @@ do
 	    output_file="$2"
 	    shift
 	    ;;
-	-h|--help)
-	    echo "$help_text"
-	    exit 0
-	    ;;
 	*)
             # process any other argument not specified above.
 	    # cannot recognise when such an argument also has a value e.g. -f <filename>
-	    # but will treat that value as another (hopefully unrecognised) argument, so this is okay
-	    echo "Warning: unrecognised option $key will be ignored." >&2
+	    # but we'll treat that value as another (hopefully unrecognised) argument.
+	    echo "$0 : Warning: unrecognised option $key will be ignored." >&2
 	    ;;
     esac
-
     shift # this is the "default" shift
 done
 
@@ -98,16 +97,16 @@ done
 
 # check input file
 if [ -z $input_file ]; then
-    echo 'Error: an input filename must be specified with option -i.' >&2
+    echo "$0 :Error: an input filename must be specified with option -i." >&2
     exit 1
 elif [ ! -s $input_file ]; then
-    echo "Error: input alignment file $input_file is either empty or does not exist. Exiting." >&2
+    echo "$0 : Error: input alignment file $input_file is either empty or does not exist. Exiting." >&2
     exit 3
 fi
 
-# check that rf is a (positive) integer and not empty
+# check that rf is a (positive) nonzero integer and not empty
 if ! [[ $rf =~ ^[1-9]+$ ]]; then 
-	echo "Error: receptive field value must be a positive integer. You supplied '$rf'. Exiting." >&2
+	echo "$0 : Error: receptive field value must be a positive integer. You supplied '$rf'. Exiting." >&2
 	exit 2
 fi
 
@@ -138,12 +137,10 @@ case $model_type in
 	cov21stats_options='-p'
 	;;
     *)
-	echo "Error: unrecognised model type: '$model_type'. Please see usage by running $0 -h." >&2
+	echo "$0 : Error: unrecognised model type: '$model_type'. Please see usage by running $0 -h." >&2
 	exit 4
 	;;
 esac
-
-# TODO: add an option to provide a list of input filenames, and iterate over them with a given model
 
 model_file=${model_dir}/$nndef
 param_file=${model_dir}/$params
@@ -151,15 +148,18 @@ param_file=${model_dir}/$params
 # Determine if user-specified receptive_field is valid before going any further
 # For valid model_type (checked above), this check should fail iff there isn't a model with the requested RF size.
 if [ ! -s $model_file ] || [ ! -s $param_file ]; then
-    echo "Error: No model available for specified receptive field size ${rf}. Please see DeepCov documentation/paper for valid values." >&2
+    echo "$0 : Error: No model available for specified receptive field size ${rf}. Please see DeepCov documentation/paper for valid values." >&2
     exit 5
 fi
 
 ######### Inputs are (likely) okay; proceed with running
 
 # symlink nndef and params for correct pipeline
-# create symlink to nndef.py and .npz file for current arch, removing any file in cwd with the same names                                                                                                                                                                         
-ln -sf ${model_file} $nndef
+# create symlink to nndef.py and .npz file for current arch, removing any file in cwd with the same names
+# nndef needs to be in $deepcov_root, and params needs to be in $PWD.
+nndef_linkname=${deepcov_root}/$nndef
+
+ln -sf ${model_file} $nndef_linkname
 ln -sf ${param_file} $params
 
 # get correct version of input tensor from cov21stats. If cov21stats fails, stop
@@ -169,7 +169,7 @@ $cov21stats $cov21stats_options $input_file $filename_21stats
 # Actually run DeepCov
 echo 'Running DeepCov...'
 export THEANO_FLAGS='device=cpu,floatX=float32' #, blas.ldflags="-L/lib64 -lopenblas"'
-$python $predictor $filename_21stats > $con_file
+$python $deepcov_root/$predictor $filename_21stats > $con_file
 unset THEANO_FLAGS
 
 echo 'Cleaning up...'
@@ -177,6 +177,6 @@ echo 'Cleaning up...'
 rm $filename_21stats
 
 # cleanup symlinks and one .pyc file
-rm -rf $nndef $params ${nndef}c '__pycache__'
+rm -rf $nndef_linkname $params ${nndef_linkname}c "${deepcov_root}/__pycache__/"
 
 echo "Results in $con_file"
